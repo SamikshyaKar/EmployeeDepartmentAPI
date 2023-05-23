a921b5d231b586ef45341ce40e8015c5d87fb9cd
SamikshyaKar <Write2samikshyakar@gmail.com>
2023-05-23 15:40:45 +0530

    next commit

--- EmployeeDepartmentAPI/Models/AppDbContext.cs
+++ EmployeeDepartmentAPI/Models/AppDbContext.cs
@@ -1,6 +1,55 @@
-﻿namespace EmployeeDepartmentAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDepartmentAPI.Models
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions):base(dbContextOptions)
+        {
+
+
+        }
+
+        public DbSet<Employee> Employeesdb { get; set; }
+        public DbSet<Departments> Departmentsdb { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Departments>().HasData(new Departments { DepartmentID = 1, DepartmentName = "IT" });
+            modelBuilder.Entity<Departments>().HasData(new Departments { DepartmentID = 2, DepartmentName = "HR" });
+            modelBuilder.Entity<Departments>().HasData(new Departments { DepartmentID = 3, DepartmentName = "Security" });
+            modelBuilder.Entity<Departments>().HasData(new Departments { DepartmentID = 4, DepartmentName = "Finance" });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee { DateofBirth = DateTime.Now,
+            DepartmentID =1,EmployeeID=1,FirstName="samikshya1",Lastname="Kar",Gender=Gender.Female
+            
+            });
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                DateofBirth = DateTime.Now,
+                DepartmentID = 1,
+                EmployeeID = 2,
+                FirstName = "samikshya2",
+                Lastname = "Kar",
+                Gender = Gender.Female
+
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                DateofBirth = DateTime.Now,
+                DepartmentID = 2,
+                EmployeeID = 2,
+                FirstName = "samikshya3",
+                Lastname = "Kar",
+                Gender = Gender.Male
+
+            });
+        }
+
+       
+
     }
 }
--- EmployeeDepartmentAPI/Models/Employee.cs
+++ EmployeeDepartmentAPI/Models/Employee.cs
@@ -6,11 +6,11 @@
         public int DepartmentID { get; set; }
         public string FirstName { get; set; }
         public string Lastname { get; set; }
-        public string Gender { get; set; }
+        public Gender Gender { get; set; }
         public string Photopath { get; set; }
         public DateTime DateofBirth { get; set; }
         public Departments Department { get; set; }
-        public Gender gender { get; set; }
+       
 
     }
 }
