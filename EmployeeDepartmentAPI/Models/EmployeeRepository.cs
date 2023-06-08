21d9e18a1747fb3f177241494f4cf37dd79b1eb1
SamikshyaKar <Write2samikshyakar@gmail.com>
2023-06-08 18:38:10 +0530

    Added Create and Update employee

---
+++ EmployeeDepartmentAPI/Models/EmployeeRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDepartmentAPI.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+        public async Task<Employee> CreateEmployee(Employee employee)
+        {
+          var result= await appDbContext.Employeesdb.AddAsync(employee);
+                      await appDbContext.SaveChangesAsync();
+                      return result.Entity;
+        }
+
+        public async Task DeleteEmployee(int empid)
+        {
+            var result = await appDbContext.Employeesdb.FirstOrDefaultAsync(e => e.EmployeeID == empid);
+            if(result !=null)
+            {
+                appDbContext.Employeesdb.Remove(result);
+                appDbContext.SaveChanges();
+            }
+        }
+
+        public Task<IEnumerable<Employee>> GetAllEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeById(int empid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> SearchEmployee(string name, Gender? gender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmployeeDepartmentAPI/Models/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeDepartmentAPI.Models
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> GetEmployeeById(int empid);
+        Task<Employee> GetEmployeeByName(string name);
+        Task<Employee> GetEmployeeByEmail(string email);
+        Task<Employee> CreateEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<IEnumerable<Employee>> GetAllEmployee();
+        Task DeleteEmployee(int empid);
+
+        Task<IEnumerable<Employee>> SearchEmployee(string name, Gender? gender);
+
+
+    }
+}
