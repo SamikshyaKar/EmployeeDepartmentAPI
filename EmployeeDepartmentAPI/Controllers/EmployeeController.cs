467e0c2daf73915f961bb20a132ca8de92284ed9
SamikshyaKar <Write2samikshyakar@gmail.com>
2023-06-09 18:11:55 +0530

    updated appsettings , got data from apis

--- EmployeeDepartmentAPI/Controllers/EmployeeController.cs
+++ EmployeeDepartmentAPI/Controllers/EmployeeController.cs
@@ -5,7 +5,7 @@ namespace EmployeeDepartmentAPI.Controllers
 {
     [ApiController]
     [Route("[Controller]")]
-    public class EmployeeController:ControllerBase
+    public class EmployeeController : ControllerBase
     {
         private readonly IEmployeeRepository employeeRepository;
 
@@ -14,6 +14,39 @@ namespace EmployeeDepartmentAPI.Controllers
             this.employeeRepository = employeeRepository;
         }
 
+        [HttpGet]
+        public async Task<ActionResult> GetAllEmployeesList()
+        {
+            var result = await employeeRepository.GetAllEmployee();
+
+            if (result != null) return Ok(result);
+            else return BadRequest();
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var result = await employeeRepository.GetEmployeeById(id);
+            if (result != null) return Ok(result);
+            else return BadRequest();
+        }
+
+        [HttpGet("EmployeeName")]
+        public async Task<IActionResult> GetEmployeebyName(string employeebyName)
+        {
+            try
+            {
+                //var result = await employeeRepository.GetEmployeeByName(employeebyName.ToLower().ToString());
+                //if (result != null) return Ok(result);
+                return Ok(await employeeRepository.GetEmployeeByName(employeebyName.ToLower().ToString()));
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving from database");
+            }
+
+
+        }
 
 
     }
