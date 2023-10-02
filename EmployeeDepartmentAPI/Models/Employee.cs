4134b53e11e6345256b8d20e145a79a096d43bb6
SamikshyaKar <Write2samikshyakar@gmail.com>
2023-10-02 17:27:14 +0530

    next commit

--- EmployeeDepartmentAPI/Models/Employee.cs
+++ EmployeeDepartmentAPI/Models/Employee.cs
@@ -4,14 +4,22 @@ namespace EmployeeDepartmentAPI.Models
 {
     public class Employee
     {
+        // Employee class
+        // Employee class properties
+
+        //EmployeeID
         public int EmployeeID { get; set; }
+        //DepartmentID
         public int DepartmentID { get; set; }
         [MaxLength(18, ErrorMessage = "First Name Requires at max 18 characters")]
         [MinLength(2,ErrorMessage ="First Name Requires at Least 2 characters")]
+        //FirstName
         public string FirstName { get; set; }= string.Empty;
         [MaxLength(10, ErrorMessage = "Last Name Requires at max 10 characters")]
         [MinLength(2, ErrorMessage = "Last Name Requires at Least 2 characters")]
+        //Lastname
         public string Lastname { get; set; } = string.Empty;
+        //Gender enum
         public Gender Gender { get; set; }=Gender.Female;
         public string Photopath { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
